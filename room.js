d3f5e384d2469e717ec10530f12291335418d605
sou <sasaki.sou@samurai-adways.net>
2022-01-27 21:01:53 +0900

    react

--- room.js
+++ room.js
@@ -1,35 +1,57 @@
 fetch("http://localhost:3000/rooms/1/members")
-    .then((res) => res.json())
-    .then((data) => {
-        const cards = document.querySelector("#members");
-        cards.innerHTML = "";
-        data.forEach((el) => {
-            cards.appendChild(
-                htmlToElement(`
+  .then((res) => res.json())
+  .then((data) => {
+    const cards = document.querySelector("#members");
+    cards.innerHTML = "";
+    data.forEach((el) => {
+      cards.appendChild(
+        htmlToElement(`
                 <div>
                     ${el.name}
                 </div>
             `)
-            );
-        });
+      );
     });
+  });
 
 fetch("http://localhost:3000/is-valid-token")
-    .then((res) => res.json())
-    .then((data) => {
-        if (data.value === true) {
-            const joinDiv = document.querySelector("#join-div");
-            joinDiv.innerHTML = "";
-            joinDiv.appendChild(
-                htmlToElement(`
+  .then((res) => res.json())
+  .then((data) => {
+    if (data.value === true) {
+      const joinDiv = document.querySelector("#join-div");
+      joinDiv.innerHTML = "";
+      joinDiv.appendChild(
+        htmlToElement(`
                     <div id="waiting-info">Waiting for other members...</div>
                 `)
-            )
-        }
-    })
+      );
+    }
+  });
 
-// TODO: form submit
-document.addEventListener()
+(function () {
+  const form = document.querySelector("#join-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const inputs = form.elements;
+    const name = inputs["name"].value;
+    fetch("http://localhost:3000/members", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        roomId: 1,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        // TODO: refetch members here.
+            // or listen some endpoint like /changed, and refetch all APIs above when the server push something from /changed.
+      });
+  });
+})();
 
 /**
  * reference: https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro/35385518#35385518
@@ -37,8 +59,8 @@ document.addEventListener()
  * @return {Element}
  */
 function htmlToElement(html) {
-    var template = document.createElement("template");
-    html = html.trim(); // Never return a text node of whitespace as the result
-    template.innerHTML = html;
-    return template.content.firstChild;
+  var template = document.createElement("template");
+  html = html.trim(); // Never return a text node of whitespace as the result
+  template.innerHTML = html;
+  return template.content.firstChild;
 }
