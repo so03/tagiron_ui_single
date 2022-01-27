b0d3093684f2f21403fbcc54f475de59b56ee489
sou <sasaki.sou@samurai-adways.net>
2022-01-27 22:17:48 +0900

    add: cards

--- express/main.js
+++ express/main.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const app = express();
+const api = express.Router();
+api.use(express.json());
 
-app.get('/api/rooms/1/members', (req, res) => {
-    res.send(JSON.stringify([
+api.get('/rooms/1/members', (req, res) => {
+    res.json([
         {
             name: "sasaki"
         }
-    ]))
+    ])
 })
+
+app.get('/api/cards', (req, res) => {
+    res.json([
+        {
+            color: "red",
+            number: 1,
+        },
+        {
+            color: "blue",
+            number: 1,
+        },
+        {
+            color: "yellow",
+            number: 5,
+        },
+        {
+            color: "red",
+            number: 8,
+        }
+    ])
+})
+
+app.use('/api', api);
 
 app.listen(3030);
--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import axiosBase from "axios";
+
+const axios = axiosBase.create({
+  baseURL: "http://localhost:3000/api",
+});
 
 function App() {
   return (
     <>
+      <Card.Cards />
       <Members />
-      hello
     </>
   );
 }
@@ -16,7 +20,7 @@ function Members() {
   };
   const [members, setMembers] = useState<null | Member[]>(null);
   useEffect(() => {
-    axios.get("/api/rooms/1/members").then((res) => {
+    axios.get("/rooms/1/members").then((res) => {
       setMembers(res.data);
     });
   }, []);
@@ -26,11 +30,58 @@ function Members() {
   }
   return (
     <>
-      {members.map((member) => (
-        <p>{member.name}</p>
+      {members.map((member, i) => (
+        <p key={i}>{member.name}</p>
       ))}
     </>
   );
+}
+
+namespace Card {
+  type Card = {
+    color: string;
+    number: number;
+  };
+
+  export function Cards() {
+    const [cards, setCards] = useState<null | Card[]>(null);
+    useEffect(() => {
+      axios.get("/cards").then((res) => setCards(res.data));
+    }, []);
+
+    if (!cards) {
+      return null;
+    }
+    return (
+      <div className="flex gap-4">
+        {cards.map((card, i) => (
+          <Card key={i} card={card} />
+        ))}
+      </div>
+    );
+  }
+
+  type CardProps = {
+    card: Card;
+  };
+
+  function Card({ card }: CardProps) {
+    const color = cardColor(card);
+
+    return <div className={`p-4 cursor-pointer ${color}`}>{card.number}</div>;
+  }
+
+  function cardColor(card: Card) {
+    let color = null;
+    if (card.color === "red") {
+      color = "text-red-400";
+    } else if (card.color === "blue") {
+      color = "text-blue-400";
+    } else if (card.color === "yellow") {
+      color = "text-yellow-400";
+    }
+    return color;
+  }
 }
 
 export default App;
